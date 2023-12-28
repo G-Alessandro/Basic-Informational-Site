366e3b5382b330bdab844603fa9de335cd9622f3
G-Alessandro <galloalessandro77@gmail.com>
2023-12-28 20:07:30 +0100

    Code rewritten using Express

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
-
+const app = express();
 const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-  let filePath = `./public${req.url}.html`;
-
-  if (req.url === '/') {
-    filePath = './public/index.html';
-  }
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        filePath = './public/404.html';
-        fs.readFile(filePath, (err, data) => {
-          if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-          } else {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end(data);
-          }
-        });
-      } else {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Erro');
-      }
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    }
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-server.listen(PORT, () => {
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'contact-me.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'))
+});
+
+app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
